c7362dbc2864cc5a80bc56c1b979c2a772ee7e11
noe gateway vm <noe.merles@gmail.com>
2024-01-09 13:08:48 +0100

    added nvim-tree, changed remap to packer

--- lua/noe/packer.lua
+++ lua/noe/packer.lua
@@ -1,4 +1,17 @@
 -- This file can be loaded by calling `lua require('plfind_filesugins')` from your init.vim
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 --
 -- -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
@@ -35,7 +48,16 @@ return require('packer').startup(function(use)
         }
     }
     use {
+        'nvim-tree/nvim-tree.lua',
+        requires = {
+            'nvim-tree/nvim-web-devicons',
+        },
+    }
+    use {
         'nvim-lualine/lualine.nvim',
         requires = { 'nvim-tree/nvim-web-devicons', opt = true }
     }
+if packer_bootstrap then
+require('packer').sync()
+end
 end)
